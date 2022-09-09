0d2f75d2a77985247a47a16e0f3d9a766c18f791
Bruno Campos <bruno.mcamps@outlook.com>
2022-09-08 22:41:05 -0300

    Added ICollection Produtos

--- src/Accreditaire.Business/Models/Fornecedores/Fornecedor.cs
+++ src/Accreditaire.Business/Models/Fornecedores/Fornecedor.cs
@@ -1,4 +1,5 @@
 ﻿using Accreditaire.Business.Core.Models;
+using Accreditaire.Business.Models.Produtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,20 @@ using System.Threading.Tasks;
 
 namespace Accreditaire.Business.Models.Fornecedores
 {
-    class Fornecedor : Entity
+    public class Fornecedor : Entity
     {
+        public string Nome { get; set; }
+        
+        public string Documento { get; set; }
+
+        public TipoFornecedor TipoFornecedor { get; set; }
+
+        public Endereco Endereco { get; set; }
+
+        public bool Ativo { get; set; }
+
+        /* EF Relations */
+
+        public ICollection<Produto> Produtos { get; set; }
     }
 }
---
+++ src/Accreditaire.Business/Models/Fornecedores/TipoFornecedor.cs
@@ -0,0 +1,8 @@
+﻿namespace Accreditaire.Business.Models.Fornecedores
+{
+    public enum TipoFornecedor
+    {
+        PessoaFisica = 1,
+        PessoaJuridica
+    }
+}
